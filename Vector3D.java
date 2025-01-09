e0f6636353cf237a327efcd9db7c9dde2e64c2dc
Soham Nighojkar <sohamnigh@gmail.com>
2025-01-09 15:28:30 -0800

    Added Vector3D

---
+++ Vector3D.java
@@ -0,0 +1,27 @@
+public class Vector3D {
+
+    public double x;
+    public double y;
+    public double z;
+
+    // problem 1
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    // problem 2
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+}
